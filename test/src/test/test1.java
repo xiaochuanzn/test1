c25c91cbe8fa16ee9294c2b0bcd1dfe4d02ae048
xc <xc@LAPTOP-EVMQKUA1>
2020-02-07 16:14:55 +0800

    aa

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 	System.out.println("test1.main()");
 	System.out.println("aaaaaaa");
 	//22222222222222
-	///11111111111111
+	///11111111111111///2222222
 }
 }
